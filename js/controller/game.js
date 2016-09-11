883ddb7b7bafb1c0df8f77b94977fb0e8fd5693f
Samuel Santos <samuelsantosdev@gmail.com>
2016-09-11 04:47:41 -0300

    Fail message, reset game

--- js/controller/game.js
+++ js/controller/game.js
@@ -1,16 +1,80 @@
 var Game = function(engine){
 
     this.engine = engine;
+    this.time = setTimeout(function(){});
 
     this.main = function(){
         try{            
             this.screenStart();
+            this.addEvents();
         }
         catch(ex)
         {
             console.log(ex);
         }
     };
+
+    this.rulesGame = function(){
+        var mainController = this;
+        if( mainController.snakeBody.pieces[0].x < 0 || 
+            mainController.snakeBody.pieces[0].y < 0 || 
+            mainController.snakeBody.pieces[0].x > this.engine.ctx.canvas.clientWidth - 10 || 
+            mainController.snakeBody.pieces[0].y > this.engine.ctx.canvas.clientHeight - 10)
+        {
+            clearInterval(this.time);
+            var alert = this.engine.libraries.canvas.elements.TextObj;
+            alert.x          = this.engine.ctx.canvas.clientWidth / 2 - 100;
+            alert.y          = this.engine.ctx.canvas.clientHeight / 2;
+            alert.bgColor    = "#fff";
+            alert.font       = "20px Arial";
+            alert.text       = "Fail - Enter to continue";
+            this.canvas.render(this.engine.ctx, alert);  
+            mainController.snakeBody.reset();
+        }
+    }
+
+    this.addEvents = function(){
+        var mainController = this;
+        var ctx = this.engine.ctx;
+        var canvas = this.canvas;
+        var s = Object.create(this.engine.entities.stage);
+        var hc = Object.create(this.engine.libraries.canvas);
+        this.engine.libraries.event.addEvent(
+            s, 
+            'keydown', 
+            this.engine.ctx.canvas, 
+            function(keyPress)
+                {
+                    console.log(keyPress.key);
+                    switch(keyPress.key){
+                        case'Enter':
+                            if(mainController.snakeBody.pieces.length == 0){
+                                canvas.clearAll(ctx);
+                                mainController.snakeBody.init(hc);
+                                mainController.time = setInterval(function(){
+                                    canvas.clearAll(ctx);
+                                    mainController.snakeBody.renderSnake(ctx, hc);
+                                    mainController.rulesGame();
+                                }, 100);
+                            }
+                        break;
+                        case'ArrowUp':
+                            mainController.snakeBody.moveUp();
+                        break;
+                        case'ArrowDown':
+                            mainController.snakeBody.moveDown();
+                        break;
+                        case'ArrowLeft':
+                            mainController.snakeBody.moveLeft();
+                        break;
+                        case'ArrowRight':
+                            mainController.snakeBody.moveRight();
+                        break;
+                    }
+
+                }
+        );
+    }
 
     this.screenStart = function(){
         try{
@@ -24,53 +88,15 @@ var Game = function(engine){
             s.width = w;
             s.height = h - 30;
             s.color = "#333";
-            s.renderStage(this.engine.ctx);
+            s.renderStage(this.engine.ctx);            
             
-            var hc = Object.create(this.engine.libraries.canvas);
             var button = this.engine.libraries.canvas.elements.TextObj;
             button.x          = w / 2 - 20;
             button.y          = h / 2;
             button.bgColor    = "#fff";
             button.font       = "20px Arial";
             button.text       = "Enter";
-            this.canvas.render(this.engine.ctx, button);
-
-            var ctx = this.engine.ctx;
-            var canvas = this.canvas;
-            this.engine.libraries.event.addEvent(
-                s, 
-                'keydown', 
-                this.engine.ctx.canvas, 
-                function(keyPress)
-                    {
-                        console.log(keyPress.key);
-                        switch(keyPress.key){
-                            case'Enter':
-                                if(mainController.snakeBody.bodySnake.length == 0){
-                                    canvas.clearAll(ctx);
-                                    mainController.snakeBody.init(hc);
-                                    setInterval(function(){
-                                        canvas.clearAll(ctx);
-                                        mainController.snakeBody.renderSnake(ctx, hc);
-                                    }, 200);
-                                }
-                            break;
-                            case'ArrowUp':
-                                mainController.snakeBody.moveUp();
-                            break;
-                            case'ArrowDown':
-                                mainController.snakeBody.moveDown();
-                            break;
-                            case'ArrowLeft':
-                                mainController.snakeBody.moveLeft();
-                            break;
-                            case'ArrowRight':
-                                mainController.snakeBody.moveRight();
-                            break;
-                        }
-
-                    }
-            );
+            this.canvas.render(this.engine.ctx, button);           
 
         }
         catch(ex)
@@ -80,10 +106,14 @@ var Game = function(engine){
     }   
 
     this.snakeBody = {
-        bodySnake : [],
+        pieces : [],
         coordinatesChange : {x:0, y:0},
         directionSnake : {x:0, y:0},
-        
+        reset : function(){
+            this.pieces = [];
+            this.coordinatesChange = {x:0, y:0};
+            this.directionSnake = {x:0, y:0};
+        },
         addPiece : function(canvas){
             snakePiece = canvas.elements.RectObj;
 
@@ -92,42 +122,44 @@ var Game = function(engine){
             snakePiece.bgColor    ="#fff";
             snakePiece.lnWidth    =2;
             snakePiece.lnColor    ="#000";
+            snakePiece.x = 0;
+            snakePiece.y = 0;
             
-            this.bodySnake.push(snakePiece);
+            this.pieces.push(snakePiece);
         },        
         renderSnake : function(ctx, hc){
            
-            if(this.bodySnake[0].x == 0 && this.bodySnake[0].y == 0){
-                this.bodySnake[0].x = ctx.canvas.clientWidth / 2; 
-                this.bodySnake[0].y = ctx.canvas.clientHeight / 2;
+            if(this.pieces[0].x == 0 && this.pieces[0].y == 0){
+                this.pieces[0].x = ctx.canvas.clientWidth / 2; 
+                this.pieces[0].y = ctx.canvas.clientHeight / 2;
             }
 
-            for(var i = 0; i < this.bodySnake.length; i++){
+            for(var i = 0; i < this.pieces.length; i++){
                 
-                this.bodySnake[i].x = this.bodySnake[i].x - this.coordinatesChange.x;
-                this.bodySnake[i].y = this.bodySnake[i].y - this.coordinatesChange.y;
+                this.pieces[i].x = this.pieces[i].x - this.coordinatesChange.x;
+                this.pieces[i].y = this.pieces[i].y - this.coordinatesChange.y;
                 
-                hc.render(ctx, this.bodySnake[i]);
+                hc.render(ctx, this.pieces[i]);
             }
         },
         moveLeft : function(){
-            this.coordinatesChange.x = 10;
+            this.coordinatesChange.x = 5;
             this.coordinatesChange.y = 0;
         },
         moveRight : function(){
-            this.coordinatesChange.x = -10;
+            this.coordinatesChange.x = -5;
             this.coordinatesChange.y = 0;
         },
         moveUp : function(){
             this.coordinatesChange.x = 0;
-            this.coordinatesChange.y = 10;
+            this.coordinatesChange.y = 5;
         },
         moveDown : function(){
             this.coordinatesChange.x = 0;
-            this.coordinatesChange.y = -10;
+            this.coordinatesChange.y = -5;
         },
         init : function(hc){
-            this.bodySnake = [];
+            this.pieces = [];
             this.addPiece(hc);
             this.moveLeft();
         }
